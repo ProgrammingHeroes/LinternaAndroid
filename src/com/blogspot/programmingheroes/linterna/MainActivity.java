e32594953ad5cdd0c23566a78727e053362c2fde
Programming Heroes <programmingh@gmail.com>
2014-05-19 14:59:18 +0200

    Videotutorial 3. Encendiendo el flash.

--- src/com/blogspot/programmingheroes/linterna/MainActivity.java
+++ src/com/blogspot/programmingheroes/linterna/MainActivity.java
@@ -1,24 +1,49 @@
 
+// MainActivity.java ------------------------------------------------- 
+
 package com.blogspot.programmingheroes.linterna;
 
+
 import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity
+{
+
+	private Camera camera;
+	
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Encender el flash.
+        camera = Camera.open();
+        Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(parameters);
+        camera.startPreview();
     }
-
-
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onBackPressed()
+    {
+    	super.onBackPressed();
+    	
+    	// Apagar el flash.
+    	// No es necesario apagar el flash si vamos a cerrar la
+    	// cámara, se apaga automáticamente.
+    	//Parameters parameters = camera.getParameters();
+        //parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+        //camera.setParameters(parameters);
+    	camera.stopPreview();
+    	camera.release();
     }
 
-}
+} // fin de la clase MainActivity
+
+// fin de MainActivity.java ------------------------------------------
