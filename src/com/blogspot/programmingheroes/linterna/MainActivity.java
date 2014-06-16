18cc329efda40ec03f7fa369b0294b20f0488d15
Programming Heroes <programmingh@gmail.com>
2014-06-16 02:02:39 +0200

    Videotutorial 6. Notificación "en curso".

--- src/com/blogspot/programmingheroes/linterna/MainActivity.java
+++ src/com/blogspot/programmingheroes/linterna/MainActivity.java
@@ -5,9 +5,14 @@ package com.blogspot.programmingheroes.linterna;
 
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -25,9 +30,13 @@ public class MainActivity extends Activity implements OnClickListener
 	
 	private static final String WAKE_LOCK_TAG = "Linterna";
 	
+	private static final int NOTIFICATION_ID = 1;
+	
 	private Torch torch;
 	
 	private WakeLock wakeLock;
+	
+	private NotificationManager notificationManager;
 	
 	
     @Override
@@ -54,12 +63,45 @@ public class MainActivity extends Activity implements OnClickListener
         {
         	wakeLock.acquire();
         }
+        
+        // Iniciar el NotifactionManager.
+        notificationManager = (NotificationManager)
+        		getSystemService(NOTIFICATION_SERVICE);
+        
+        // Creamos la notificación.
+        createNotification();
+    }
+    
+    public void createNotification()
+    {
+    	Intent intent = new Intent(this, MainActivity.class);
+    	
+    	PendingIntent pendingIntent = PendingIntent.getActivity(this,
+    			0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    	
+    	Notification notification = new NotificationCompat.Builder(this)
+    		.setSmallIcon(R.drawable.ic_launcher)
+    		.setContentTitle(getResources().getString(R.string.app_name))
+    		.setContentText(getResources().getString(R.string.notification_text))
+    		.setOngoing(true)
+    		.setContentIntent(pendingIntent)
+    		.build();
+    	
+    	notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+    
+    public void destroyNotification()
+    {
+    	notificationManager.cancel(NOTIFICATION_ID);
     }
     
     @Override
     public void onBackPressed()
     {
     	super.onBackPressed();
+    	
+    	// Destruir notifiación.
+    	destroyNotification();
     	
     	// Apagar el flash.
     	// No es necesario apagar el flash si vamos a cerrar la
@@ -76,10 +118,12 @@ public class MainActivity extends Activity implements OnClickListener
 		if (torch.isOn())
 		{
 			torch.off();
+			destroyNotification();
 		}
 		else
 		{
 			torch.on();
+			createNotification();
 		}
 	}
 
